d217906bde919978b1091d5c7277098fba0a4d61
GabrielBornato <gabriel.bornato@grvppe.com>
2019-10-02 18:48:50 -0300

    Aplicação OmniStack - criando servidor

---
+++ backend/src/server.js
@@ -0,0 +1,27 @@
+    const express =  require('express');
+    const routes = require('./routes');
+    const app = express();
+
+    //Para filtros
+
+    // app.get('/users', (req,res)=>{
+    //     return res.json({message: req.query.idade})
+    // })
+
+    //Para pegar o corpo da requisição
+
+    // app.use(express.json());
+    // app.post('/users', (req,res)=>{
+    //     return res.json({message: req.body})
+    // });
+
+    //Para edição ou delete
+
+    // app.put('/users/:id', (req,res)=>{
+    //     return res.json({message: req.params.id});
+    // });
+
+    app.use(express.json());
+    app.use(routes)
+
+    app.listen(3333);
